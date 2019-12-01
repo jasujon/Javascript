0640cc2ceacbfee07dcf9fa5687d5ccd83084420
jasujon <jubayedalamsujon@gmail.com>
2019-12-01 17:48:19 +0600

    javascript Variable,DataType,Function

---
+++ lession-one/lession.js
@@ -0,0 +1,39 @@
+// var name='Jubayed Alam';
+// var age=24;
+// var skill=['html','css','bootstrap'];
+// // console.log(typeof skill);
+
+// var something;
+// var anything=null;
+
+// console.log(something === anything);
+
+// console.log(name,age,skill);
+// console.log(name);
+// console.log(age);
+// console.log(skill);
+
+
+// var age=13;
+// if (age >= 20) {
+// 	console.log('You can it');
+// }else{
+// 	console.log('Sorry,You are under 20');
+// }
+
+// var name='sujon';
+
+// for (var i=0; i<20; i++) {
+// 	console.log(name);
+// }
+
+// function add(a,b){
+// 	return a+b;
+// 	// var c=a+b;
+// 	// return c;
+// }
+
+var add=function(a,b){
+	return a+b;
+}
+console.log(add(10,20));
