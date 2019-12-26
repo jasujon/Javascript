6a8aab1d15d0830a9f2058476bcbf3739601f92a
jasujon <jubayedalamsujon@gmail.com>
2019-12-26 15:01:54 +0600

    ES6 - Template String And Multi line String

--- ES6/src/scripts/index.js
+++ ES6/src/scripts/index.js
@@ -8,3 +8,13 @@ import '../style/index.scss';
 // arr.push(10);
 // arr.push(20);
 // console.log(arr);
+
+/*---------------------------------ES6 - Template String And Multi line String---------------------*/
+
+const name ='Jubayed Alam';
+const age = 25
+const email = 'jubayedalamsujon@gmail.com';
+
+// const details = 'Name: ' +name + ' Age: ' +age + ' Email: ' +email;
+const details = `name: ${name} ; age : ${age} ; email : ${email}`
+console.log(details);
